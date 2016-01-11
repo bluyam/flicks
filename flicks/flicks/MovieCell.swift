5a6badd4a2e4f28b3ad80b415208716b3f6b0c80
bluyam <bluyam@tamu.edu>
2016-01-10 20:18:11 -0600

    completed basic features from tutorials

---
+++ flicks/flicks/MovieCell.swift
@@ -0,0 +1,29 @@
+//
+//  MovieCell.swift
+//  flicks
+//
+//  Created by Kyle Wilson on 1/10/16.
+//  Copyright © 2016 Bluyam Inc. All rights reserved.
+//
+
+import UIKit
+
+class MovieCell: UITableViewCell {
+
+    @IBOutlet var titleLabel: UILabel!
+    @IBOutlet var overviewLabel: UILabel!
+    
+    @IBOutlet var posterImageView: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
